cdf9344a0132f17948a065effc9f55764541b327
rubenvg16 <rvilgut193@g.educaand.es>
2025-01-20 10:32:18 +0100

    a

--- src/main/java/org/iesvegademijas/tienda_informatica/controlador/ProductoController.java
+++ src/main/java/org/iesvegademijas/tienda_informatica/controlador/ProductoController.java
@@ -1,5 +1,16 @@
 package org.iesvegademijas.tienda_informatica.controlador;
 
+import org.iesvegademijas.tienda_informatica.modelo.Producto;
+import org.iesvegademijas.tienda_informatica.servicio.FabricanteService;
+import org.iesvegademijas.tienda_informatica.servicio.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class ProductoController {
 
+    @Autowired
+    private ProductoService productoService;
 }
